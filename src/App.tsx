c356bcb34c506218e0e3bcf5489bc17b69566f2d
TeoryMan <jonathancortstm143@gmail.com>
2023-04-16 13:58:22 -0500

    Overflow hiden

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MainContent from './interface/MainContent';
 
 function App() {
     return (
-        <div className='flex flex-col min-h-screen'>
+        <div className='flex flex-col min-h-screen overflow-hidden'>
             <Header />
             <MainContent />
         </div>
