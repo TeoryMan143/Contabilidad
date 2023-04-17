de472ea804bd2eaa2fb2cb9b45b0ce9a15f72736
TeoryMan <jonathancortstm143@gmail.com>
2023-04-17 17:32:14 -0500

    Button animation

--- src/components/DefButton.tsx
+++ src/components/DefButton.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { SubsContext } from '../context/SubsContext';
+
 interface Props {
     children: string;
     icon?: string | JSX.Element;
@@ -5,12 +8,22 @@ interface Props {
 }
 
 function DefButton({ children, icon, onClick }: Props) {
+    const { showSubs } = useContext(SubsContext);
+
     return (
         <button
-            className='bg-coral-red-500 text-white text-lg font-semibold p-4 rounded-full hover:bg-coral-red-400 active:translate-y-0.5 transition-all flex items-center'
+            className='bg-coral-red-500 text-white text-lg font-semibold p-4 rounded-full hover:bg-coral-red-400 active:translate-y-0.5 flex items-center'
             onClick={onClick}
         >
-            {icon && <span className='mr-3 text-3xl '>{icon}</span>}
+            {icon && (
+                <span
+                    className={`mr-3 text-3xl transition-all ${
+                        showSubs && '-rotate-90'
+                    }`}
+                >
+                    {icon}
+                </span>
+            )}
             <span>{children}</span>
         </button>
     );
